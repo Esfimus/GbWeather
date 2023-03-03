3820cfc37e0165cf9c3abd033d1cc02d9b376c9f
Nikolay N. Nikolaev <esfimus@gmail.com>
2023-03-03 16:23:25 +0300

    Service loads weather from API, result is presented with Toast.

--- app/src/main/java/com/esfimus/gbweather/domain/broadcast/BroadcastService.kt
+++ app/src/main/java/com/esfimus/gbweather/domain/broadcast/BroadcastService.kt
@@ -7,6 +7,9 @@ import android.os.IBinder
 import android.widget.Toast
 import com.esfimus.gbweather.data.WEATHER_LOCATION_EXTRA
 import com.esfimus.gbweather.domain.Location
+import com.esfimus.gbweather.domain.WeatherView
+import com.esfimus.gbweather.domain.api.LoadWeather
+import com.esfimus.gbweather.domain.api.Loadable
 
 class BroadcastService : Service() {
 
@@ -17,11 +20,32 @@ class BroadcastService : Service() {
         } else {
             intent?.getParcelableExtra(WEATHER_LOCATION_EXTRA)
         }
-        Toast.makeText(
-            this,
-            "Location: ${location?.name} ${location?.lat} ${location?.lon}",
-            Toast.LENGTH_SHORT).show()
+        updateWeather(location ?: Location("North Pole", 90.0, 0.0))
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun updateWeather(location: Location) {
+        val loadableWeather: Loadable = object : Loadable {
+            override fun loaded(weather: WeatherView) {
+                toast("""
+                    ${weather.location.name}
+                    ${weather.currentTimeView}
+                """.trimIndent())
+            }
+            override fun failed(responseCode: Int) {
+                when (responseCode) {
+                    in 300 until 400 -> toast("Redirection")
+                    in 400 until 500 -> toast("Client Error")
+                    in 500 until 600 -> toast("Server Error")
+                }
+            }
+        }
+        val loader = LoadWeather(location, loadableWeather)
+        loader.loadWeather()
+    }
+
+    private fun toast(text: String) {
+        Toast.makeText(this,text,Toast.LENGTH_LONG).show()
     }
 
     override fun onBind(intent: Intent): IBinder? {
